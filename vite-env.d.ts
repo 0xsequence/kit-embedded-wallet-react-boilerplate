7cd4e0081972c5bfc34b216d81f4e1d807750e9b
samuelea <batonenbois@gmail.com>
2024-07-08 12:48:13 -0400

    VITE_APP_TITLE read only

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  VITE_APP_TITLE: string
+  readonly VITE_APP_TITLE: string
   readonly VITE_KIT_ACCESS_KEY: string
   readonly VITE_WAAS_CONFIG_KEY: string
   readonly VITE_GOOGLE_CLIENT_ID: string
