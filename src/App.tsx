eb7251906b10d4c2ef60dbe1c98fb7e402fb22d8
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2024-07-25 00:01:45 -0700

    fixing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Home from './views/Home'
 import { KitProvider } from '@0xsequence/kit'
 import { getDefaultWaasConnectors } from '@0xsequence/kit-connectors'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Transport } from 'viem'
 import { createConfig, http, WagmiProvider } from 'wagmi'
 import { mainnet, polygon, Chain } from 'wagmi/chains'
 
@@ -12,7 +11,7 @@ const App = () => {
   const chains = [mainnet, polygon] as [Chain, ...Chain[]]
 
   // Get your own keys on sequence.build
-  const projectAccessKey = import.meta.env.VITE_PROJECT_ACCESS_KEY || 'AQAAAAAAAEGvyZiWA9FMslYeG_yayXaHnSI'
+  const projectAccessKey = import.meta.env.VITE_ || 'AQAAAAAAAEGvyZiWA9FMslYeG_yayXaHnSI'
   const waasConfigKey = import.meta.env.VITE_WAAS_CONFIG_KEY || 'eyJwcm9qZWN0SWQiOjE2ODE1LCJlbWFpbFJlZ2lvbiI6ImNhLWNlbnRyYWwtMSIsImVtYWlsQ2xpZW50SWQiOiI2N2V2NXVvc3ZxMzVmcGI2OXI3NnJoYnVoIiwicnBjU2VydmVyIjoiaHR0cHM6Ly93YWFzLnNlcXVlbmNlLmFwcCJ9'
   const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '970987756660-35a6tc48hvi8cev9cnknp0iugv9poa23.apps.googleusercontent.com'
   const appleClientId = import.meta.env.VITE_APPLE_CLIENT_ID || 'com.horizon.sequence.waas'
@@ -32,7 +31,7 @@ const App = () => {
     projectAccessKey
   })
 
-  const transports: { [key: number]: Transport } = {}
+  const transports: { [key: number]: any } = {}
 
   chains.forEach(chain => {
     transports[chain.id] = http()
