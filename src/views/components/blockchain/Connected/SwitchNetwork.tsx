fe3c97fe471165773a30de8ba6ca47b1da897be5
Richard Miguel Irala <richardirala47@hotmail.com>
2024-08-05 15:10:17 -0300

    Added switch network funtionality into UI

---
+++ src/views/components/blockchain/Connected/SwitchNetwork.tsx
@@ -0,0 +1,26 @@
+import { Select } from "@0xsequence/design-system";
+import { Chain } from "viem";
+import { useSwitchChain } from "wagmi";
+
+const SwitchNetwork = (props: { chain: Chain }) => {
+  const { chain } = props;
+  const { chains, switchChainAsync } = useSwitchChain();
+  const onSwitchChain = async (chainId: string) => {
+    const formmatedChainId = Number(chainId);
+    await switchChainAsync({ chainId: formmatedChainId });
+  };
+
+  return (
+    <Select
+      onValueChange={onSwitchChain}
+      name="switchChain"
+      options={chains?.map((chain) => ({
+        label: chain.name,
+        value: chain.id.toString(),
+      }))}
+      defaultValue={chain?.id?.toString()}
+    />
+  );
+};
+
+export default SwitchNetwork;
