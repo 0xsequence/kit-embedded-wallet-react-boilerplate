059f11f9c7f01a2a10479dc91e7f100fb60bd0f7
Richard Miguel Irala <richardirala47@hotmail.com>
2024-08-05 15:10:17 -0300

    Added switch network funtionality into UI

--- src/views/components/blockchain/Connected/ChainInfo.tsx
+++ src/views/components/blockchain/Connected/ChainInfo.tsx
@@ -2,12 +2,22 @@ import { Box } from "@0xsequence/design-system";
 import { Address, Chain } from "viem";
 import ActiveNetwork from "./ActiveNetwork";
 import NativeBalance from "./NativeBalance";
+import SwitchNetwork from "./SwitchNetwork";
 
 const ChainInfo = (props: { chain: Chain; address: Address }) => {
   const { chain, address } = props;
+
   return (
     <Box marginBottom="8">
-      <ActiveNetwork chain={chain} />
+      <Box
+        display="flex"
+        gap="4"
+        justifyContent="space-between"
+        alignItems="flex-end"
+      >
+        <ActiveNetwork chain={chain} />
+        <SwitchNetwork chain={chain} />
+      </Box>
       <NativeBalance chain={chain} address={address} />
     </Box>
   );
