9b154e84c00d17049bd4bbc06d55b8c42eb21fd0
moskalyk <morgan.moskalyk@protonmail.ch>
2024-10-03 11:55:04 -0400

    updated default chainId

--- src/config.ts
+++ src/config.ts
@@ -10,8 +10,8 @@ const walletConnectId = import.meta.env.VITE_WALLET_CONNECT_ID;
 
 export const config: any = createConfig('waas', {
     projectAccessKey: projectAccessKey,
-    chainIds: [1, 137],
-    defaultChainId: 1,
+    chainIds: [1, 421614],
+    defaultChainId: 421614,
     appName: 'Kit Starter',
     waasConfigKey: waasConfigKey,
     googleClientId: googleClientId,
