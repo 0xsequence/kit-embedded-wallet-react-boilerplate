18d776dae632ab12dff0af7392913a60f07dccab
moskalyk <morgan.moskalyk@protonmail.ch>
2024-10-03 11:53:59 -0400

    updates

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+import { createConfig } from '@0xsequence/kit';
+
+// Get your own keys on sequence.build
+const projectAccessKey = import.meta.env.VITE_PROJECT_ACCESS_KEY;
+const waasConfigKey = import.meta.env.VITE_WAAS_CONFIG_KEY;
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const appleClientId = import.meta.env.VITE_APPLE_CLIENT_ID;
+const appleRedirectURI = window.location.origin + window.location.pathname;
+const walletConnectId = import.meta.env.VITE_WALLET_CONNECT_ID;
+
+export const config: any = createConfig('waas', {
+    projectAccessKey: projectAccessKey,
+    chainIds: [1, 137],
+    defaultChainId: 1,
+    appName: 'Kit Starter',
+    waasConfigKey: waasConfigKey,
+    googleClientId: googleClientId,
+    appleClientId: appleClientId,
+    appleRedirectURI: appleRedirectURI,
+    walletConnectProjectId: walletConnectId
+})
