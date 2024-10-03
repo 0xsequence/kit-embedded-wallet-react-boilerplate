cc6dab6a28eef99208c6f74e898e901cad1c86f0
moskalyk <morgan.moskalyk@protonmail.ch>
2024-10-03 11:53:59 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,13 @@
 import Home from "./Home";
-import { getDefaultWaasConnectors, KitProvider } from "@0xsequence/kit";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createConfig, http, WagmiProvider } from "wagmi";
-import chains from "./constants";
+import { SequenceKit } from "@0xsequence/kit";
+import { config } from './config'
 import "@0xsequence/design-system/styles.css";
-const queryClient = new QueryClient();
 
 const App = () => {
-  // Get your own keys on sequence.build
-  const projectAccessKey = import.meta.env.VITE_PROJECT_ACCESS_KEY;
-  const waasConfigKey = import.meta.env.VITE_WAAS_CONFIG_KEY;
-  const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-  const appleClientId = import.meta.env.VITE_APPLE_CLIENT_ID;
-  const appleRedirectURI = window.location.origin + window.location.pathname;
-  const walletConnectId = import.meta.env.VITE_WALLET_CONNECT_ID;
-
-  const connectors = getDefaultWaasConnectors({
-    walletConnectProjectId: walletConnectId,
-    waasConfigKey,
-    googleClientId,
-    // Notice: Apple Login only works if deployed on https (to support Apple redirects)
-    appleClientId,
-    appleRedirectURI,
-    /* Arbitrum sepolia chainId */
-    defaultChainId: 421614,
-    appName: "Kit Starter",
-    projectAccessKey,
-  });
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const transports: { [key: number]: any } = {};
-
-  chains.forEach((chain) => {
-    transports[chain.id] = http();
-  });
-
-  const config = createConfig({
-    transports,
-    connectors,
-    chains,
-  });
-
-  const kitConfig = {
-    projectAccessKey,
-  };
-
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <KitProvider config={kitConfig}>
-          <Home />
-        </KitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <SequenceKit config={config}>
+      <Home />
+    </SequenceKit>
   );
 };
 
